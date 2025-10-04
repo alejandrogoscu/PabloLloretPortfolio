dbe357a3beaaab9d1293656771b2386747d6e373
alejandrogoscu <alejandrogoscu@gmail.com>
2025-10-04 18:59:11 +0200

    config for render

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/admin', adminRoutes);
