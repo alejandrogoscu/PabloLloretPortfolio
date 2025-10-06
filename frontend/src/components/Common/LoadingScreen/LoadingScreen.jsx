c9f389dc04f621d3f4e79921457a69d5da565968
alejandrogoscu <alejandrogoscu@gmail.com>
2025-10-06 10:42:52 +0200

    ok logic and styles LoadingScreen

--- frontend/src/components/Common/LoadingScreen/LoadingScreen.jsx
+++ frontend/src/components/Common/LoadingScreen/LoadingScreen.jsx
@@ -4,8 +4,15 @@ import './loadingScreen.css';
 const LoadingScreen = () => {
   return (
     <div className="loading-screen">
-      <img className="loading-logo" src="../../../../public/images/favicon/PLfavicon.ico" alt="logo"></img>
-      <Spinner />
+      <div className="loading-container">
+        <div className="loading-content">
+          <h1 className="loading-title">Pablo Lloret</h1>
+          <p className="loading-subtitle">Video Editor</p>
+          <div className="loading-spinner">
+            <Spinner />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
